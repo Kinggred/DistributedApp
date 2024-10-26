ca1bdb631b260409bec8d578cfcb6017742e62c6
Maksymilian Grupiński <maks.grupinski@solve.md>
2024-10-26 13:24:20 +0200

    Refectored loggers

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"log"
+	"context"
 	"sync"
 	config "tic-tac-toe/core/config"
 	networking "tic-tac-toe/core/networking"
+	"time"
 )
 
 func main() {
-
 	config.LoadVariables()
+	config.SetupLoggers()
+
+	log := config.AppLogger
+
+	ctx, cancel := context.WithCancel(context.Background())
+
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	// TODO: Start server on command of a user
 	go func() {
 		defer wg.Done()
-		log.SetPrefix("Server: ")
 		log.Printf("Trying to start the Lobby")
 		networking.RunServer()
 	}()
@@ -24,13 +29,17 @@ func main() {
 	if config.CONFIG.SERVER_ONLY == false {
 		go func() {
 			defer wg.Done()
-			log.SetPrefix("Client: ")
 			log.Printf("Trying to start a client")
-			networking.RunClient()
+			networking.RunClient(ctx)
 		}()
 	} else {
 		defer wg.Done()
 	}
 
+	// Test
+	time.Sleep(60 * time.Second)
+	cancel()
+
 	wg.Wait()
+
 }
